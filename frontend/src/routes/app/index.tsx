6d09d4cb52e603822265c7327b1104b6a5e37b7b
ItDaaG <ghuasaliofficial@gmail.com>
2025-04-12 12:18:37 +0100

    Update index.tsx

--- frontend/src/routes/app/index.tsx
+++ frontend/src/routes/app/index.tsx
@@ -1,12 +1,16 @@
 import Dashboard from '@/components/dashboard/dashboard';
 import { createFileRoute } from '@tanstack/react-router';
-import { ProtectedRoute, useProtectedRoute } from '@/context/ProtectedRouteContext';
+import { ProtectedRoute } from '@/context/ProtectedRouteContext';
 
 // We can define beforeMatch if we need more complex route protection logic
 export const Route = createFileRoute('/app/')({
     component: ProtectedAppRoute,
 });
 
-function RouteComponent() {
-    return <Dashboard>app</Dashboard>;
+function ProtectedAppRoute() {
+    return (
+        <ProtectedRoute>
+            <Dashboard>app</Dashboard>
+        </ProtectedRoute>
+    );
 }
